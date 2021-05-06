12e39229a5fad32f5bae55cbc869dae6fac194ed
sakina lougue <70714610+slougue1@users.noreply.github.com>
2021-05-05 20:40:12 -0400

    ThreadLap

--- Lab/CarQueue.java
+++ Lab/CarQueue.java
@@ -11,8 +11,8 @@ public class CarQueue {
    PriorityQueue<Integer> q = new PriorityQueue<Integer>();
    private int out;
   
-   public CarQueue(){
-  
+   public CarQueue()
+   { 
        q.add(ran.nextInt(4));
        q.add(ran.nextInt(4));
        q.add(ran.nextInt(4));
@@ -25,21 +25,16 @@ public class CarQueue {
        q.add(ran.nextInt(4));
        q.add(ran.nextInt(4));
        q.add(ran.nextInt(4));
-       q.add(ran.nextInt(4));
-      
-      
-  
+       q.add(ran.nextInt(4));             
    }
   
-   public void addToQueue() {
-  
+   public void addToQueue() 
+   {  
        class ARunnable implements Runnable
-          {
-          
+          {          
              public void run()
-             {
-              
-                try
+             {              
+            	 try
                 {
                    q.add(ran.nextInt(4));
                    q.add(ran.nextInt(4));
@@ -47,38 +42,31 @@ public class CarQueue {
                    q.add(ran.nextInt(4));
                    q.add(ran.nextInt(4));
                    q.add(ran.nextInt(4));
-                   q.add(ran.nextInt(4));
-                  
-                  
+                   q.add(ran.nextInt(4));                                    
                 }
                 finally
-                {
-                  
+                {                  
                 }
              }
           }
        Runnable r = new ARunnable();
-       Thread t = new Thread(r);
-       
+       Thread t = new Thread(r);      
           t.start();  
    }
 
-   public int deleteQueue() {
-      
+   public int deleteQueue() 
+   {     
        class ARunnable implements Runnable
-          {
-          
-          
+          {                 
              public void run()
              {
-
                try {
                        Thread.sleep(2000);
-                   } catch (InterruptedException e) {
+                   } catch (InterruptedException e) 
+               {
                       
                    }
-                  
-              
+                               
                 try
                 {
                    if(q.size() < 20){
@@ -95,15 +83,12 @@ public class CarQueue {
                        q.add(ran.nextInt(4));
                        q.add(ran.nextInt(4));
                        q.add(ran.nextInt(4));
-                       q.add(ran.nextInt(4));
-                  
-                   }
-                  
+                       q.add(ran.nextInt(4));                  
+                   }                  
                    else if (!q.isEmpty()){
                    out = q.remove();
                    }
-                  
-                  
+                                    
                 }
                 finally
                 {
@@ -113,14 +98,11 @@ public class CarQueue {
            
           }
        Runnable r = new ARunnable();
-       Thread t = new Thread(r);
-       
+       Thread t = new Thread(r);       
           t.start();
-        
-  
+          
           return out;
       
    }
-
 }
 
